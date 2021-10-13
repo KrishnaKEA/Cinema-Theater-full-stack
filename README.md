4fcee5838cc206c6087bf7057cd89d3d0561ad3a
omar <omar1807@stud.kea.dk>
2021-10-13 09:59:50 +0200

    1st commit about Movie API

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# cinema-app by KODAK
+# springboot-movies-rest-api
+Spring Boot REST API for other applications in our company.
+
+To replicate this back-end application in your local environment:
+
+1) - Make sure to install the following softwares mentioned below:
+
+Java SE Development Kit 11: https://www.oracle.com/java/technologies/downloads/#java11
+
+Apache Maven: https://maven.apache.org/download.cgi
+
+IntelliJ Idea Ultimate IDE: https://www.jetbrains.com/idea/download/#section=windows (note: this is a paid version, however you can use the free Community version with limited capability though)
+
+
+2) - MySQL Server is essential to perform the database operations.
+
+3) - Clone the repository to your local workspace
+
+4) - Import the project to your IntelliJ IDE
+
+5) - Update the application.properties file with the SQL Database information according to your environment.
+
+6) - I hard coded some dummy data, in my case 3 kind of Movies, by using the 'CommandLine Runner' in the Main class.
+
+7) - Open command prompt in the project folder and run the following command mentioned below to start the REST API:
+
+  mvn spring-boot:run
+
+8) - Now, let's test it using 'Postman' or 'Browser' and visit:
+    
+      http://localhost:8082/movies           ==> API to get all the movies
+    
+      http://localhost:8082/movies/id        ==> API to get a specific movie by id
+
+      http://localhost:8082/movies/new       ==> API to add a new Movie
+
+      http://localhost:8082/movies/update/id ==> API to update/edit Movie by id
+
+      http://localhost:8082/movies/delete/id ==> API to delete specific movie by id
--- src/main/java/kodak/cinemaapp/CinemaAppApplication.java
+++ src/main/java/kodak/cinemaapp/CinemaAppApplication.java
@@ -1,13 +1,43 @@
 package kodak.cinemaapp;
 
+import kodak.cinemaapp.entities.hall.Hall;
+import kodak.cinemaapp.entities.hall.HallRepo;
+import kodak.cinemaapp.entities.movie.Movie;
+import kodak.cinemaapp.entities.movie.MovieRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CinemaAppApplication {
+public class CinemaAppApplication implements CommandLineRunner {
+
+    @Autowired
+    MovieRepo movieRepo;
+    @Autowired
+    HallRepo hallRepo;
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaAppApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Hall hall1 = new Hall("Panorama Hall");
+        Hall hall2 = new Hall("Luxurious Hall");
+
+        hallRepo.save(hall1);
+        hallRepo.save(hall2);
+
+        Movie movie1 =  new Movie("Fate of the furious","With Dom and Letty married, Brian and Mia retired and the rest of the crew exonerated, the globe-trotting team has found some semblance of a normal life", "High","Vin Diesel and Tyrese Gibson",hall1);
+        Movie movie2 = new Movie("Titanic","James Cameron is 'Titanic' is an epic, action-packed romance set against the ill-fated maiden voyage of the R.M.S. Titanic; the pride and joy of the White Star Line and, at the time, the largest moving object ever built.", "High", " Leonardo DiCaprio and Kate Winslet", hall2);
+        Movie movie3 = new Movie("Mission Impossible"," It is a multimedia franchise based on a fictional secret espionage agency known as the Impossible Missions Force (IMF). The 1966 TV series ran for 7 seasons and was revived in 1988 for two seasons. It inspired a series...", "High","Tom Cruise and Ethan Hunt", hall1);
+
+        movieRepo.save(movie1);
+        movieRepo.save(movie2);
+        movieRepo.save(movie3);
+    }
 }
+
+
