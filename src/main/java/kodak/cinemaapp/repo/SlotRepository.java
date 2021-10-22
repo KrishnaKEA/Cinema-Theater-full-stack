37713f8b007f493423d3944e911aec375c29d288
Karolina Urn <64137994+shpomp@users.noreply.github.com>
2021-10-22 11:21:17 +0200

    Revert "Krishna branch"

--- src/main/java/kodak/cinemaapp/repo/SlotRepository.java
+++
@@ -1,7 +0,0 @@
-package kodak.cinemaapp.repo;
-
-import kodak.cinemaapp.entities.Slot;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SlotRepository extends CrudRepository<Slot,Integer> {
-}
